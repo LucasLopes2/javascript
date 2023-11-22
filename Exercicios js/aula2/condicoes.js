2bbb5b457a0cffe7a8dad77eeb11461e69182d2a
Lucas lopes <lucasgithub403@gmail.com>
2023-11-21 16:24:54 -0800

    exercicios de condicoes em java script

---
+++ Exercicios js/aula2/condicoes.js
@@ -0,0 +1,6 @@
+var vel = 100
+console.log(`A velocidade da sua máquina é ${vel} Km/H`)
+if (vel > 60){ //CONDIÇAO SIMPLES//
+console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança`)
