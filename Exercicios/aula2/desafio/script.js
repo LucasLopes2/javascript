b3e4dc9b8f08fcd023ef9e5b0f57ae5cea5fecd0
Lucas lopes <lucasgithub403@gmail.com>
2023-12-13 23:42:25 -0800

    praticando a logica de programação js

--- Exercicios/aula2/desafio/script.js
+++ Exercicios/aula2/desafio/script.js
@@ -1,22 +1,18 @@
 function carregar(){
-var msg = window.document.getElementById("msg")
-var img = window.document.getElementById("imagem")
-var data = new Date()
-var hora = data.getHours()
-msg.innerHTML = `Agora são ${hora} horas.`
-
-if (hora >= 8 && hora < 12){
-    img.src = 'imgs/tarde.jpg'
-    document.body.style.background = "#B7752B"
-}else if(hora >= 12 && hora <= 18){
-   img.src = 'imgs/tarde2.jpg'
-   document.body.style.background = "#FC8064"
-}else{
-    img.src = 'imgs/noite.jpg'
-    document.body.style.background = '#233740'
+    var msg = document.getElementById("msg")
+    var img = document.getElementById("imagem")
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas`
+    if(hora >=8 && hora <12){
+        img.src = 'imgs/tarde.jpg'
+        document.body.style.background = '#B7752B'
+    }else if (hora >=12 && hora <18){
+        img.src = 'imgs/tarde2.jpg'
+        document.body.style.background ='#FC8064'
+    }else{
+        img.src ='imgs/noite.jpg'
+        document.body.style.background = '#233740'
+    }
 }
-
-
-}
-
 
--- Exercicios/aula3/desafio3/tabuada.js
+++ Exercicios/aula3/desafio3/tabuada.js
@@ -1,4 +1,4 @@
-function tabuada (){
+function tabuada(){
     var num = document.getElementById("txtn")
     var tabu = document.getElementById("seltab")
     if(num.value.length == 0){
@@ -7,9 +7,9 @@ function tabuada (){
         let n = Number(num.value)
         let c = 1
         tabu.innerHTML = ''
-        while(c <=10){
-            let item = document.createElement('option')
-            item.text = `${n} X ${c} = ${n*c}`
+        while(c <= 10){
+            let item = document.createElement("option")
+            item.text = `${n} x ${c} = ${n*c}`
             tabu.appendChild(item)
             c++
         }
