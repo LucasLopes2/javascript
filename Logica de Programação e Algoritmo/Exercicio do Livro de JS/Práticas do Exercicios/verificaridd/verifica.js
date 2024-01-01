ad51acf6cce1d9b9600d4425333e2b580bc1d6b3
Lucas Lopes Santos <144072041+LucasLopes2@users.noreply.github.com>
2024-01-01 15:09:32 -0800

    verficador

--- Logica de Programação e Algoritmo/Exercicio do Livro de JS/Práticas do Exercicios/verificaridd/verifica.js
+++ Logica de Programação e Algoritmo/Exercicio do Livro de JS/Práticas do Exercicios/verificaridd/verifica.js
@@ -1,51 +1,44 @@
 function verificar(){
     var data = new Date()
-    var ano =  data.getFullYear()
-    var dano = document.getElementById('nasci')
-    var res = document.getElementById('res')
-    if(dano.value.length == 0){
-        alert("{ERRO} Verifique seus dados e tente novamente!")
+    var anual = data.getFullYear()
+    var info = document.getElementById("nasci")
+    var res = document.getElementById("res")
+    if(info.value.length == 0){
+        alert("Por favor , digite o seu ano de nascimento!")
     }else{
-        var radf = document.getElementsByName("rads")
-        var idade = ano - Number(dano.value)
-        var genero = ''
-        var img = document.createElement('img')
-         img.setAttribute('id', 'foto')
-    }
-    if(radf[0].checked){
-        genero = 'Homem'
-        if(idade >= 0 && idade < 10){
-            //kid//
-            img.setAttribute('src', 'foto-kidm.jpg')
-        }else if(idade < 21) {
-            //jovem//
-            img.setAttribute("src", 'foto-adolece.jpg')
-        }else if(idade < 50){
-            //adulto
-            img.setAttribute('src', 'foto-adulto.jpg')
-        }else{
-            //idoso/
-            img.setAttribute('src', 'foto-idoso.jpg')
-        }
-    }else if(radf[1].checked){
-        genero = 'Mulher'
-        if(idade >= 0 && idade < 10){
-            //kid//
-            img.setAttribute('src','foto-kidf.jpg')
-        }else if(idade <= 21) {
-            //jovem//
-            img.setAttribute('src', 'foto-adole (2).jpg')
-        }else if(idade < 50){
-            //adulto
-            img.setAttribute('src', 'foto-adulta.jpg')
-        }else{
-            //idoso/
-            img.setAttribute('src', 'foto-idosa.jpg')
-        }
+        var radf = document.getElementsByName("sexo")
+        var genero  = ''
+        var idade = anual - Number(info.value)
+        var img = document.createElement("img")
+        img.setAttribute('id','foto')
 
-       
-    } 
-    res.style.textAlign = 'center'
-    res.innerHTML = `Detectamos ${genero} de ${idade} anos.`
+        if(radf[0].checked){
+            genero = 'Homem'
+            if(idade >=0 && idade < 12){
+                img.setAttribute("src", 'foto-kidm.jpg')
+            }else if(idade <= 18){
+                img.setAttribute("src", 'foto-adolece.jpg')
+            }else if(idade <= 45){
+                img.setAttribute("src", 'foto-adulto.jpg')
+            }else{
+                img.setAttribute("src", 'foto-idoso.jpg')
+            }
+        }else if(radf[1].checked){
+            genero = 'Mulher'
+            if(idade >=0 && idade < 12){
+                img.setAttribute("src", 'foto-kidf.jpg')
+            }else if(idade <=18){
+                img.setAttribute("src", 'foto-adole (2).jpg')
+            }else if(idade <=45){
+                img.setAttribute("src", 'foto-adulta.jpg')
+            }else{
+                img.setAttribute("src", 'foto-idosa.jpg')
+            }
+        }
+    
+    res.style.textAlign = "Center"
+    res.innerHTML = `Detectamos ${genero} com ${idade} anos`
     res.appendChild(img)
+    }
 }
+    
