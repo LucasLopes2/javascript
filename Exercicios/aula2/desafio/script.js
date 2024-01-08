c9ed989e73a7e4b51e479f32b12586967f65c100
Lucas Lopes Santos <144072041+LucasLopes2@users.noreply.github.com>
2024-01-08 19:28:08 -0300

    treinando exercicio de java script do curso

--- Exercicios/aula2/desafio/script.js
+++ Exercicios/aula2/desafio/script.js
@@ -1,18 +1,17 @@
 function carregar(){
-    var msg = document.getElementById("msg")
-    var img = document.getElementById("imagem")
-    var data = new Date()
-    var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas`
-    if(hora >=8 && hora <12){
+    let data = new Date()
+    let hora = data.getHours()
+    let img =  document.getElementById("imagem")
+    let mensage = document.getElementById("msg")
+    mensage.innerHTML = `Agora são ${hora} horas`
+    if(hora >= 8 && hora <12){
         img.src = 'imgs/tarde.jpg'
-        document.body.style.background = '#B7752B'
-    }else if (hora >=12 && hora <18){
+        document.body.style.background = "#B7752B"
+    }else if(hora >=12 && hora <18){
         img.src = 'imgs/tarde2.jpg'
-        document.body.style.background ='#FC8064'
+        document.body.style.background = "#FC8064"
     }else{
-        img.src ='imgs/noite.jpg'
-        document.body.style.background = '#233740'
+        img.src = 'imgs/noite.jpg'
+        document.body.style.background = "#233740"
     }
 }
-
